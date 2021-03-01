5a83c5936bca2a8f7ca0b450454efd03784b6554
Gustavo Cruz <gu.cruz17@hotmail.com>
2021-02-28 21:14:53 -0300

    Project

---
+++ src/components/Profile.tsx
@@ -0,0 +1,21 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
+
+import styles from '../styles/components/Profile.module.css';
+
+export function Profile() {
+    const { level } = useContext(ChallengesContext);
+
+    return (
+        <div className={styles.profileContainer}>
+            <img src="https://github.com/GustavoSC1.png" alt="Gustavo Cruz"/>
+            <div>
+                <strong>Gustavo Cruz</strong>
+                <p>
+                    <img src="icons/level.svg" alt="Level"/>
+                    Level {level}
+                </p>
+            </div>
+        </div>
+    );
+}
